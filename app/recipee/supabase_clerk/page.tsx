55469971ccdd92f6f15245e01e37ad51d2525602
Wayne Kaminsky <waykay@Waynes-MBP.broadband>
2025-03-04 19:38:15 +0000

    Clerk5

--- app/recipee/supabase_clerk/page.tsx
+++ app/recipee/supabase_clerk/page.tsx
@@ -2,11 +2,13 @@
 import { useAuth } from "@clerk/nextjs";
 import { supabase } from "@/utils/supabaseClient";
 
+// TODO: To test UPDATE YOUR PRESONAL DATA on the supabase dashboard not the clerk dashboard
+
 export default function MyComponent() {
   const { getToken } = useAuth();
 
   const fetchData = async () => {
-    const token = await getToken({ template: "supabase" }); // Get the Supabase JWT
+    const token = await getToken({ template: "supabase_recipee" }); // Get the Supabase JWT
 
     // Pass the token in the headers for this specific request
     const { data, error } = await supabase
